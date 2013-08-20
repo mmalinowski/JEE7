c34d00bf500462746836e4ec952049f377be47fd
mmalinowski <mmalinowski83@gmail.com>
2013-08-20 21:46:43 +0200

    SOme java docs added

--- WebSocket/backend/websocket-service-api/src/main/java/com/malinowski/jee7/websocket/logic/UserService.java
+++ WebSocket/backend/websocket-service-api/src/main/java/com/malinowski/jee7/websocket/logic/UserService.java
@@ -15,14 +15,38 @@ import com.malinowski.jee7.websocket.model.User;
 public interface UserService {
 
 	/**
+	 * JMS topic.
+	 */
+	String NOTIFICATION_TOPIC = "/jms/websocket/notifications";
+
+	/**
+	 * Returns list of all existing users.
 	 * 
-	 * @return
+	 * @return list of all existing users.
 	 */
 	List<User> findAll();
 
+	/**
+	 * Creates a new user in the database. When a user is created, a notification is published on {@code #NOTIFICATION_TOPIC}.
+	 * 
+	 * @param user
+	 *            a user to be created. An id will be generated automatically.
+	 */
 	void create(User user);
 
+	/**
+	 * Updates an existing user in the database. When a user is updated, a notification is published on {@code #NOTIFICATION_TOPIC}.
+	 * 
+	 * @param user
+	 *            a user to be updated.
+	 */
 	void update(User user);
 
+	/**
+	 * Deletes a user from the database. When a user is deleted, a notification is published on {@code #NOTIFICATION_TOPIC}.
+	 * 
+	 * @param user
+	 *            a user to be deleted.
+	 */
 	void delete(User user);
 }
